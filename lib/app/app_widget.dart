f46a4855f310271a08ad58af63576e85d16b18d8
jrgirotto <jrgirotto@live.com>
2022-02-10 18:41:39 -0300

    Aula SALVAR TASKS

---
+++ lib/app/app_widget.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:todo_list_provider/app/core/database/sqlite_admin_connection.dart';
+import 'package:todo_list_provider/app/core/navigator/todo_list_navigator.dart';
+import 'package:todo_list_provider/app/core/ui/todo_list_ui_config.dart';
+import 'package:todo_list_provider/app/modules/auth/auth_module.dart';
+import 'package:todo_list_provider/app/modules/home/home_module.dart';
+import 'package:todo_list_provider/app/modules/splash/splash_page.dart';
+import 'package:todo_list_provider/app/modules/tasks/tasks_module.dart';
+
+class AppWidget extends StatefulWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  State<AppWidget> createState() => _AppWidgetState();
+}
+
+class _AppWidgetState extends State<AppWidget> {
+  final sqliteAdmConnection = SqliteAdminConnection();
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance?.addObserver(sqliteAdmConnection);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance?.removeObserver(sqliteAdmConnection);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Todo List Provider',
+      theme: TodoListUIConfig.theme,
+      navigatorKey: TodoListNavigator.navigatorKey,
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [
+        Locale('pt', 'BR'),
+      ],
+      routes: {
+        ...AuthModule().routers,
+        ...HomeModule().routers,
+        ...TasksModule().routers,
+        
+      },
+      home: SplashPage(),
+    );
+  }
+}
---
+++ lib/app/core/database/migrations/migration_V3.dart
@@ -0,0 +1,21 @@
+import 'package:sqflite_common/sqlite_api.dart';
+import 'package:todo_list_provider/app/core/database/migrations/migration.dart';
+
+class MigrationV3 implements Migration {
+  @override
+  void create(Batch batch) {
+    batch.execute('''
+      create table todo3(
+        id Integer primary key autoincrement,
+        descricao varchar(200) not null,
+        data_hora datetime,
+        finalizado integer
+      )
+    ''');
+  }
+
+  @override
+  void update(Batch batch) {
+  }
+  
+}
